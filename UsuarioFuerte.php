ef5a7f29f1e9d57ac25b5e0de7e2ceee50ba1492
Fragmo <marcferper@gmail.com>
2018-04-28 13:46:39 +0200

    empezando calendario

--- UsuarioFuerte.php
+++ UsuarioFuerte.php
@@ -1,36 +1,202 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
-        <title>TODO supply a title</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="Css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+        <title>Hitbee</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maxium-scale=1.0, minimun-scale =1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="js/jquery.raty.css" rel="stylesheet" type="text/css"/>
+        <link href="css/propioCss.css" rel="stylesheet" type="text/css"/>
+        <link href="css/icomoon.css" rel="stylesheet" type="text/css"/>
+        <!--vh relacion con el alto vw relacion con el ancho de la pagina-->
         <script src="js/jquery.js" type="text/javascript"></script>
-        <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery.raty.js" type="text/javascript"></script>
         
-        <style>
-            .rojo a{background: red !important; background-image: none !important;}
-        </style>
+        <link rel='stylesheet' href='css/calendario/fullcalendar.css' />
+        <script src='js/calendario/jquery.min.js'></script>
+        <script src='js/calendario/moment.min.js'></script>
+        <script src='js/calendario/fullcalendar.js'></script>
+        <script src="js/calendario/es.js" type="text/javascript"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     </head>
-    <body>
-       
-        <div class="row">
-            <div class="col-md-3" id="calendario"></div>
-            <div class="col-md-6">divs para abrir los programas de entremaniento, dieta etc</div>
-            <div class="col-md-3">ranking usuarios</div>
-        </div>
+    <body class="body-wrap-home">
+        <?php 
+        include './metodos.php';
+        $miMetodos = new metodos();
+        $nivel = "";
+        $tipo = $_GET['tipo'];
+        $Modo = $tipo[0];
+        if(isset($_GET['usuarioNuevo'])){
+            echo '<script language="javascript">alert("Usuario registrado con éxito");</script>';
+        }
+        ?>
         
-
-        <script>
-            var disabledDays = ['2017-10-21',
-                                '2017-10-22'];
-            var diasOcupados =  ['2017-10-12',
-                                '2017-10-15'];
+        
             
+        <header>
+        <div class="row container" >
             
-            $("#calendario").datepicker();
-         
+            <div class="menu_bar  col-xs-2 col-s-3 col-md-3 col-lg-3">
+                <a href="#" class="bt-menu iconoMenu"><span class="icon-bars"></span></a>
+            </div>
+            
+            <div class=" header-logo col-lg-3">
+                <h1>
+                    <a a href="index.php" id="logo"></a>
+                </h1>
+            </div>
+            
+            <div class="col-xs-7 col-s-6 col-md-6  col-lg-6" style="text-align: center">
                 
-        </script>
-    </body>
+                <div id="HitBee">
+                   
+                </div>
+                
+                <div  id="menu">
+                    <ul class="top-menu">
+                        <li><a href="index.php"><span class="li-text"> Workouts </span></a></li>
+                        <li><a href="Productos.php"><span class="li-text"> Noticias </span></a></li>
+                        <li><a href="Productos.php"><span class="li-text"> Dietas </span></a></li>
+                        <li><a href="Productos.php"><span class="ac_unit"> Info </span></a></li>
+                        
+                    </ul>
+                </div>
+                
+                
+            </div>
+            
+            <div id="top-user" class="col-xs-3 col-s-3 col-md-3  col-lg-3">
+            
+                <div id="marginLogin">
+                    <a href="javascript:void(0)" id="loginPop" title="Login" data-toggle="modal" data-target="#pop-auth">
+                       <!--al pinchar en este boton se abre el modal puesto en el data-target-->
+                       <i class="icon-user-circle iconoLogin"  data-toggle="modal" data-target="#ModalContenedor"></i> <span class="letraLogin">LOGIN</span>
+                    </a>                
+                    
+                    
+                    
+                </div>
+            
+            
+            
+            
+            </div>
+            
+    </header>
+       
+    <!--hace que el body se baje-->
+    <div class="headerPad" ></div>
+    
+    
+                <nav class="navResponsive">
+                    <ul class="ulResponsive">
+                        <li class="liResponsive"><a class="aResponsive" href="#"><span class="icon-house spanResponsive"></span>Inicio</a></li>
+                        <li class="liResponsive"><a class="aResponsive" href="#"><span class="icon-suitcase spanResponsive"></span>Trabajos</a></li>
+                        <li class="liResponsive"><a class="aResponsive" href="#"><span class="icon-earth spanResponsive"></span>Servicios</a></li>
+                        <li class="liResponsive"><a class="aResponsive" href="#"><span class="icon-mail spanResponsive"></span>Contacto</a></li>
+                    </ul>
+		</nav>
+    
+    
+        <!--****MODAAAAAAAAAAAAAAAAAAAAAL*****-->
+<!-- Modal -->
+<div class="modal fade" id="ModalContenedor" tabindex="-1" role="dialog" aria-labelledby="tituloModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+          <!--Este boton es la x de arriba a la derecha-->
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+          <h3 class="modal-title text-center"  id="tituloModal"><b>Formulario de registro</b></h3>
+      </div>
+        
+      <div class="modal-body">
+          <form name="formularioRegsitroModal" action="registro.php" method="POST">
+          <table border="0" style=" margin-left: auto; margin-right: auto;">
+                    <tbody>
+                      <tr>
+                          <td>Nombre de usuario</td>
+                          <td style="margin-left: 10%;"><input type="text" required="" name="nombre" placeholder="Nombre de usuario" /></th>
+                      </tr>
 
+                  
+                      <tr>
+                          <td>Contraseña</td>
+                          <td style="margin-left: 10%;"> <input type="password" required="" name="contra" placeholder="contraseña" /></td>
+                      </tr>
+                      <tr>
+                          <td>Confirmar contraseña</td>
+                          <td style="margin-left: 10%;"><input type="password" required="" name="contra2" placeholder="Confirma contraseña" /></td>
+                      </tr>
+                  </tbody>
+              </table>
+          <br>
+          <input  type="submit" class="btn  btn-block btn-primary"value="Confirmar" name="botonEnviar" />   
+          </form>
+      </div>
+    </div>
+  </div>
+</div>
+        
+<div class="container">   
+    <div class="col-md-5" data-toggle="modal" data-target="#modalEvento1" id="calendar"></div>
+</div>      
+
+
+<script>
+$("#calendar").fullCalendar({
+    dayClick:function(date,jsEvent,view){
+        $(this).css('background-color', 'red');
+        alert('valor seleccionado: ' + date.format());
+//        $('#modalEvento1').modal();
+    },
+    eventSources:[{
+            events:[
+        {
+            title:'titulo',
+            descripcion:'descripcion',
+            start:'2018-05-01'
+            
+        }
+    ]//event
+    }],//eventSources
+    eventClick:function(calEvent,jsEvent,view){
+        $('#titutloEvento').html(calEvent.title);
+        $('#descripcionEvento').html(calEvent.descripcion);
+    }
+});
+</script>
+<!--MODAAAAAAAAAL-->
+<!-- Modal -->
+<div class="modal fade" id="modalEvento1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="titutloEvento">Agregar titulo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <div id="descripcionEvento"></div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-secondary">Borrar</button>
+        <button type="button" class="btn btn-primary">Modificar</button>
+        <button type="button" class="btn btn-success">Guardar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </body>
 </html>
