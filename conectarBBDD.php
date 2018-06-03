c32c620ff01a7b90384129448a791a0a689695bb
Fragmo <marcferper@gmail.com>
2018-06-03 14:18:37 +0200

    arreglado eñes y tildes

--- conectarBBDD.php
+++ conectarBBDD.php
@@ -15,5 +15,5 @@
 ?>
 
 <?php
-$pdo = new PDO('mysql:dbname=proyectofinal;host=127.0.0.1','root','');
+$pdo = new PDO('mysql:dbname=proyectofinal;host=127.0.0.1','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
